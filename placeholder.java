b54351d370e0087280386ddc5c698e2edbed1ca4
Vivian Zou <67518516+vivianzo@users.noreply.github.com>
2024-04-28 19:35:42 -0400

    Add files via upload

---
+++ placeholder.java
@@ -0,0 +1,10 @@
+class placeHolder{
+  void message() {
+    System.out.println("this is a place holder file!");
+  }
+
+  void howToAccess() {
+    System.out.println("as per Northeastern class policy, I am not allowed to publicly release " +
+          "the code, please contact me to see the original");
+  }
+}
