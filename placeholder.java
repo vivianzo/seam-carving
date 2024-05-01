19ac579e7702738aa7c3b3afee8bf2deb6f8a5ae
Vivian Zou <67518516+vivianzo@users.noreply.github.com>
2024-05-01 13:51:02 -0400

    Update placeholder.java

--- placeholder.java
+++ placeholder.java
@@ -7,4 +7,12 @@ class placeHolder{
     System.out.println("as per Northeastern class policy, I am not allowed to publicly release " +
           "the code, please contact me for the original source code");
   }
+
+  //Game Instructions:
+  //When the game runs it will automatically start carving randomly
+  //To toggle between horizontal and vertical seams, press "h" and "v" respectively
+  //From the horizontal or vertical state, go back to randomly carving seams by pressing "r"
+  //To toggle grayscale mode, press "g"
+  //To pause the seam carving press the spacebar
+  //To redo removed seams, press "z"
 }
